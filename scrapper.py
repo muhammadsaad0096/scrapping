895af7d88a95fdde1966e26957fee41f5ad12b08
muhammadsaad0096 <150955850+muhammadsaad0096@users.noreply.github.com>
2023-11-15 23:34:31 +0500

    Create scrapper.py

---
+++ scrapper.py
@@ -0,0 +1,11 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ["http://quotes.toscrape.com"]
+
+    def parse(self, response):
+        for quote in response.css("span.text"):
+            yield {
+                "quote": quote.get(),
+            }
